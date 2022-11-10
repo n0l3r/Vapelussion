9105c0161ec80a1de769df72517447cd607fa419
muhamad.120140040@student.itera.ac.id <muhamad.120140040@student.itera.ac.id>
2022-11-10 21:02:53 +0700

    Add Cart Screen

---
+++ screens/Cart.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+
+const Cart = () => {
+    return (
+        <View>
+            <Text>Cart Screen</Text>
+        </View>
+    )
+}
+
+export default Cart
