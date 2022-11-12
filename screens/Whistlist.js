d14d6c4a4772316dfd57170991e32128e19b4c19
Muhammad Rizky Fahreza Gusti <muhammad.120140071@student.itera.ac.id>
2022-11-13 01:36:49 +0700

    Create Whistlist.js

---
+++ screens/Whistlist.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+
+const Wishlist = () => {
+    return (
+        <View>
+            <Text>Wishlist Screen</Text>
+        </View>
+    )
+}
+
+export default Wishlist
