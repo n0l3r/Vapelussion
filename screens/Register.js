ec66d836b4c3ea50757ee1020e8732e9e88ac9a3
Hrtnn <mherton,120140182@student.itera.ac.id>
2022-11-27 23:57:46 +0700

    create register.js

---
+++ screens/Register.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+
+const Register = () => {
+    return (
+        <View>
+            <Text>Register Screen</Text>
+        </View>
+    )
+}
+
+export default Register
