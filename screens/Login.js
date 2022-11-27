66531255ccc40e2ef958dac3f64fcfceb3bc1ab2
Hrtnn <mherton,120140182@student.itera.ac.id>
2022-11-27 23:57:16 +0700

    create login.js

---
+++ screens/Login.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+
+const Login = () => {
+    return (
+        <View>
+            <Text>Login Screen</Text>
+        </View>
+    )
+}
+
+export default Login
