ef2b67f238d0783f341e7b8b2b8b7a0b33e589c1
muhamad.120140040@student.itera.ac.id <muhamad.120140040@student.itera.ac.id>
2022-12-03 16:56:28 +0700

    Update Home js

--- screens/Home.js
+++ screens/Home.js
@@ -1,140 +1,157 @@
-import { View, Text, StyleSheet, Image, Dimensions, ScrollView } from 'react-native'
+import { View, Text, StyleSheet, TextInput, Dimensions, ScrollView, Pressable } from 'react-native'
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 // colors
 import Colors from '../utils';
 
-const Home = () => {
+const Home = ({ navigation }) => {
+    
     return (
-        <ScrollView style={styles.container}>
-            {/* offers */}
-            <View style={styles.content}>
-                <Text style={styles.title}>Offers</Text>
-                <View style={styles.horizontalScroll}>
-                    <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} >
-                        <View style={styles.cardHorizontal}>
-                            <View style={styles.cardImgHorizontal}>
 
-                            </View>
-                        </View>
-
-                        <View style={styles.cardHorizontal}>
-                            <View style={styles.cardImgHorizontal}>
-
-                            </View>
-                        </View>
-                    </ScrollView>
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <View style={styles.inputGroup}>
+                    <Icon name="magnify" size={30} color={Colors.secondary} />
+                    <TextInput style={styles.input} placeholder="Search..." placeholderTextColor={Colors.secondary} />
                 </View>
+                <Pressable onPress={() => navigation.navigate('Cart')}>
+                    <Icon name="cart-outline" size={30} color={Colors.secondary} />
+                </Pressable>
+
             </View>
+            
+            <ScrollView style={styles.body}>
 
-            {/* Category */}
-            <View style={styles.content}>
-                <Text style={styles.title}>Category</Text>
-                <View style={styles.horizontalScroll}>
-                    <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-                        <View style={styles.cardSquare}>
-                            <View style={styles.cardHeaderSquare}>
 
+                {/* offers */}
+                <View style={styles.content}>
+                    <Text style={styles.title}>Offers</Text>
+                    <View style={styles.horizontalScroll}>
+                        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} >
+                            <View style={styles.cardHorizontal}>
+                                <View style={styles.cardImgHorizontal}>
+
+                                </View>
                             </View>
-                            <View style={styles.cardFooterSquare}>
-                                <Text style={styles.cardTitle}>Title</Text>
-                            </View>
-                        </View>
 
-                        <View style={styles.cardSquare}>
-                            <View style={styles.cardHeaderSquare}>
+                            <View style={styles.cardHorizontal}>
+                                <View style={styles.cardImgHorizontal}>
 
+                                </View>
                             </View>
-                            <View style={styles.cardFooterSquare}>
-                                <Text style={styles.cardTitle}>Title</Text>
-                            </View>
-                        </View>
-
-                        <View style={styles.cardSquare}>
-                            <View style={styles.cardHeaderSquare}>
-
-                            </View>
-                            <View style={styles.cardFooterSquare}>
-                                <Text style={styles.cardTitle}>Title</Text>
-                            </View>
-                        </View>
-
-                        <View style={styles.cardSquare}>
-                            <View style={styles.cardHeaderSquare}>
-
-                            </View>
-                            <View style={styles.cardFooterSquare}>
-                                <Text style={styles.cardTitle}>Title</Text>
-                            </View>
-                        </View>
-
-                        <View style={styles.cardSquare}>
-                            <View style={styles.cardHeaderSquare}>
-
-                            </View>
-                            <View style={styles.cardFooterSquare}>
-                                <Text style={styles.cardTitle}>Title</Text>
-                            </View>
-                        </View>
-
-                    </ScrollView>
+                        </ScrollView>
+                    </View>
                 </View>
-            </View>
 
-            {/* offers */}
-            <View style={styles.content}>
-                <Text style={styles.title}>Offers</Text>
-                <View style={styles.horizontalScroll}>
-                    <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} >
-                        <View style={styles.cardVertical}>
-                            <View style={styles.cardHeader}>
-                            </View>
-                            <View style={styles.cardFooter}>
-                                <View style={styles.cardFooterLeft}>
-                                    <Text style={styles.cardTitle}>Title</Text>
-                                    <Text style={styles.cardPrice}>$ 100</Text>
+                {/* Category */}
+                <View style={styles.content}>
+                    <Text style={styles.title}>Category</Text>
+                    <View style={styles.horizontalScroll}>
+                        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+                            <View style={styles.cardSquare}>
+                                <View style={styles.cardHeaderSquare}>
+
                                 </View>
-
-                                {/* icon heart */}
-                                <Icon name="heart-outline" size={30} color={Colors.secondary} />
-                            </View>
-                        </View>
-
-                        <View style={styles.cardVertical}>
-                            <View style={styles.cardHeader}>
-                            </View>
-                            <View style={styles.cardFooter}>
-                                <View style={styles.cardFooterLeft}>
+                                <View style={styles.cardFooterSquare}>
                                     <Text style={styles.cardTitle}>Title</Text>
-                                    <Text style={styles.cardPrice}>$ 100</Text>
                                 </View>
-
-                                {/* icon heart */}
-                                <Icon name="heart-outline" size={30} color={Colors.secondary} />
                             </View>
-                        </View>
 
-                        <View style={styles.cardVertical}>
-                            <View style={styles.cardHeader}>
-                            </View>
-                            <View style={styles.cardFooter}>
-                                <View style={styles.cardFooterLeft}>
+                            <View style={styles.cardSquare}>
+                                <View style={styles.cardHeaderSquare}>
+
+                                </View>
+                                <View style={styles.cardFooterSquare}>
                                     <Text style={styles.cardTitle}>Title</Text>
-                                    <Text style={styles.cardPrice}>$ 100</Text>
                                 </View>
-
-                                {/* icon heart */}
-                                <Icon name="heart-outline" size={30} color={Colors.secondary} />
                             </View>
-                        </View>
 
+                            <View style={styles.cardSquare}>
+                                <View style={styles.cardHeaderSquare}>
 
-                    </ScrollView>
+                                </View>
+                                <View style={styles.cardFooterSquare}>
+                                    <Text style={styles.cardTitle}>Title</Text>
+                                </View>
+                            </View>
+
+                            <View style={styles.cardSquare}>
+                                <View style={styles.cardHeaderSquare}>
+
+                                </View>
+                                <View style={styles.cardFooterSquare}>
+                                    <Text style={styles.cardTitle}>Title</Text>
+                                </View>
+                            </View>
+
+                            <View style={styles.cardSquare}>
+                                <View style={styles.cardHeaderSquare}>
+
+                                </View>
+                                <View style={styles.cardFooterSquare}>
+                                    <Text style={styles.cardTitle}>Title</Text>
+                                </View>
+                            </View>
+
+                        </ScrollView>
+                    </View>
                 </View>
-            </View>
+
+                {/* offers */}
+                <View style={styles.content}>
+                    <Text style={styles.title}>Offers</Text>
+                    <View style={styles.horizontalScroll}>
+                        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} >
+                            <View style={styles.cardVertical}>
+                                <View style={styles.cardHeader}>
+                                </View>
+                                <View style={styles.cardFooter}>
+                                    <View style={styles.cardFooterLeft}>
+                                        <Text style={styles.cardTitle}>Title</Text>
+                                        <Text style={styles.cardPrice}>$ 100</Text>
+                                    </View>
+
+                                    {/* icon heart */}
+                                    <Icon name="heart-outline" size={30} color={Colors.secondary} />
+                                </View>
+                            </View>
+
+                            <View style={styles.cardVertical}>
+                                <View style={styles.cardHeader}>
+                                </View>
+                                <View style={styles.cardFooter}>
+                                    <View style={styles.cardFooterLeft}>
+                                        <Text style={styles.cardTitle}>Title</Text>
+                                        <Text style={styles.cardPrice}>$ 100</Text>
+                                    </View>
+
+                                    {/* icon heart */}
+                                    <Icon name="heart-outline" size={30} color={Colors.secondary} />
+                                </View>
+                            </View>
+
+                            <View style={styles.cardVertical}>
+                                <View style={styles.cardHeader}>
+                                </View>
+                                <View style={styles.cardFooter}>
+                                    <View style={styles.cardFooterLeft}>
+                                        <Text style={styles.cardTitle}>Title</Text>
+                                        <Text style={styles.cardPrice}>$ 100</Text>
+                                    </View>
+
+                                    {/* icon heart */}
+                                    <Icon name="heart-outline" size={30} color={Colors.secondary} />
+                                </View>
+                            </View>
 
 
-        </ScrollView>
+                        </ScrollView>
+                    </View>
+                </View>
+
+
+            </ScrollView>
+        </View>
     )
 }
 
@@ -145,7 +162,38 @@ const styles = StyleSheet.create({
         flex: 1,
         flexDirection: 'column',
         backgroundColor: Colors.bgDark,
-        padding: '3%',
+    },
+    body: {
+        flex: 1,
+        flexDirection: 'column',
+        backgroundColor: Colors.bgDark,
+        padding: 10,
+    },
+    header: {
+        flexDirection: 'row',
+        height: '8%',
+        backgroundColor: Colors.bgHeader,
+        paddingHorizontal: '4%',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginTop: Platform.OS === 'ios' ? -10 : 0,
+    },
+    inputGroup: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: Colors.bgDark,
+        borderRadius: 30,
+        paddingHorizontal: '2%',
+        width: '90%',
+        height: '60%',
+    },
+    input: {
+        marginLeft: '2%',
+        width: '81%',
+        fontSize: 18,
+        height: '90%',
+        color: Colors.secondary,
+        backgroundColor: Colors.bgDark,
     },
     content: {
         marginBottom: 20,
