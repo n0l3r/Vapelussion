8946eda4208dde1ec9acbf8ca04a68816610a1cd
muhamad.120140040@student.itera.ac.id <muhamad.120140040@student.itera.ac.id>
2022-12-03 16:56:38 +0700

    Update Index

--- screens/index.js
+++ screens/index.js
@@ -2,5 +2,6 @@ import Home from './Home';
 import Wishlist from './Wishlist';
 import Profile from './Profile';
 import Notification from './Notification';
+import Cart from './Cart';
 
-export { Home, Wishlist, Profile, Notification };
+export { Home, Wishlist, Profile, Notification, Cart };
