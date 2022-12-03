e81a83d2314de3700ecd8ff7ab986e7cde89b703
muhamad.120140040@student.itera.ac.id <muhamad.120140040@student.itera.ac.id>
2022-12-03 12:36:45 +0700

    Update Profile

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,11 +1,171 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, ScrollView, Dimensions } from 'react-native'
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+// colors
+import Colors from '../utils';
 
 const Profile = () => {
     return (
-        <View>
-            <Text>Profile Screen</Text>
-        </View>
+        <ScrollView style={styles.container}>
+            {/* Card Info Profile */}
+            <View style={styles.content}>
+                <View style={styles.cardHorizontal}>
+                    <View style={styles.cardImgHorizontal}>
+                    </View>
+
+                    <View style={styles.cardContentHorizontal}>
+                        <Text style={styles.cardTitle}>John Doe</Text>
+                        <Text style={styles.cardText}>08991234525</Text>
+                        <Text style={styles.cardText}>johndoe12@gmail.com</Text>
+                    </View>
+                    <View style={styles.cardFooterHorizontal}>
+                        <Icon name="pencil-outline" size={30} color={Colors.secondary} />
+                    </View>
+                </View>
+            </View>
+
+            <View style={styles.content}>
+                <View style={styles.card}>
+                    <View style={styles.cardContent}>
+                        <View style={styles.cardContentRow}>
+                            <View style={styles.cardContentLeft}>
+                                <Icon name="cog-outline" size={30} color={Colors.secondary} />
+                                <Text style={styles.menuText}>Settings</Text>
+                            </View>
+                            <View style={styles.cardContentRight}>
+                                <Icon name="chevron-right" size={30} color={Colors.secondary} />
+                            </View>
+                        </View>
+
+                        <View style={styles.cardContentRow}>
+                            <View style={styles.cardContentLeft}>
+                                <Icon name="information-outline" size={30} color={Colors.secondary} />
+                                <Text style={styles.menuText}>Information</Text>
+                            </View>
+                            <View style={styles.cardContentRight}>
+                                <Icon name="chevron-right" size={30} color={Colors.secondary} />
+                            </View>
+                        </View>
+
+                        <View style={styles.cardContentRow}>
+                            <View style={styles.cardContentLeft}>
+                                <Icon name="format-list-bulleted" size={30} color={Colors.secondary} />
+                                <Text style={styles.menuText}>Order History</Text>
+                            </View>
+                            <View style={styles.cardContentRight}>
+                                <Icon name="chevron-right" size={30} color={Colors.secondary} />
+                            </View>
+                        </View>
+
+                        <View style={styles.btnLarge}>
+                            <Text style={styles.btnLargeText}>Logout</Text>
+                        </View>
+                    </View>
+                </View>
+            </View>
+        </ScrollView>
     )
 }
 
 export default Profile
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        backgroundColor: Colors.bgDark,
+        padding: '3%',
+    },
+    content: {
+        marginBottom: 20,
+    },
+    cardHorizontal: {
+        flexDirection: 'row',
+        width: Dimensions.get('window').width - 25,
+        height: 150,
+        borderRadius: 10,
+        elevation: 5,
+        marginRight: 10,
+        backgroundColor: Colors.bgHeader,
+        padding: 15,
+    },
+    cardImgHorizontal: {
+        width: '30%',
+        height: '100%',
+        borderRadius: 5,
+        backgroundColor: Colors.secondary,
+    },
+    cardContentHorizontal: {
+        width: '60%',
+        height: '100%',
+        paddingLeft: 15,
+    },
+    cardFooterHorizontal: {
+        width: '10%',
+        height: '100%',
+        alignItems: 'flex-end',
+    },
+    cardTitle: {
+        color: Colors.light,
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+    cardText: {
+        color: Colors.secondary,
+        fontSize: 18,
+    },
+    card: {
+        width: Dimensions.get('window').width - 25,
+        height: Dimensions.get('window').height / 2,
+        borderRadius: 10,
+        elevation: 5,
+        marginRight: 10,
+        backgroundColor: Colors.bgHeader,
+        padding: 15,
+    },
+    cardContent: {
+        width: '100%',
+        height: '100%',
+    },
+    cardContentRow: {
+        paddingBottom: 10,
+        flexDirection: 'row',
+        borderBottomColor: Colors.secondary,
+        borderBottomWidth: 1,
+        marginBottom: 10,
+    },
+    cardContentLeft: {
+        width: '90%',
+        height: '100%',
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    cardContentRight: {
+        width: '10%',
+        height: '100%',
+        alignItems: 'flex-end',
+    },
+    menuText: {
+        color: Colors.light,
+        fontSize: 20,
+        fontWeight: 'bold',
+        paddingLeft: 15,
+    },
+    btnLarge: {
+        width: '100%',
+        height: 50,
+        borderRadius: 10,
+        backgroundColor: Colors.danger,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 20,
+        bottom: 0,
+        position: 'absolute',
+    },
+    btnLargeText: {
+        color: Colors.light,
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+
+})
