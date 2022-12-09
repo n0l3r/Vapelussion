e01abb0f7676e794d4f9b6ddf8cb35655d4aa93e
Muhammad Rizky Fahreza Gusti <muhammad.120140071@student.itera.ac.id>
2022-12-09 15:31:05 +0700

    Update

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const db = require('./config/db');
 const app = express();
 const port = 3000;
+
+app.use(express.json());
+
+const productRouter = require('./routes/product');
+
+app.use('/product', productRouter);
+
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
